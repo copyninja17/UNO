0f81aa21ff0302936c843f6aa892dd666fd3f0ce
copyinja17 <imprateekpradhan@gmail.com>
2021-10-03 21:05:55 +0530

    Made different files for each page

--- hostPrompt.py
+++ hostPrompt.py
@@ -1,52 +1,15 @@
-import pygame
-from src import button
+from src import config
 import subprocess, sys
-import pygame_textinput
 
-pygame.init()
-
-# create display window
-SCREEN_HEIGHT = 500
-SCREEN_WIDTH = 800
-TEXTBOX_WIDTH = 272
-TEXTBOX_HEIGHT = 35
-
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('UNO')
-
-#load button images
-createRoom = pygame.image.load('assets/textures/createroom.png').convert_alpha()
-joinRoom = pygame.image.load('assets/textures/joinroom.png').convert_alpha()
-
-#create button instances
-createButton = button.Button(100, 200, createRoom, 0.4)
-joinButton = button.Button(450, 200, joinRoom, 0.4)
-
-#game loop
-run = True
-
-while run:
-
-	screen.fill((202, 228, 241))
-	events = pygame.event.get()
-
+def display(screen, createButton, joinButton):
 	if createButton.draw(screen):
 		print('CREATE SCRIPT')
-		subprocess.Popen([sys.executable, 'src/uno_client.py', '1'],
-                                         creationflags=subprocess.CREATE_NEW_CONSOLE)
+		# subprocess.Popen([sys.executable, 'src/uno_client.py', '1'],
+		#                                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+		config.Page = 1
 
 	if joinButton.draw(screen):
 		print('JOIN SCRIPT HERE')
-		subprocess.Popen([sys.executable, 'src/uno_client.py', '0'],
-                                         creationflags=subprocess.CREATE_NEW_CONSOLE)
-
-	for event in events:
-		# quit game
-		if event.type == pygame.QUIT:
-			run = False
-
-	pygame.display.update()
-	clock.tick(60)
-
-pygame.quit()
+		# subprocess.Popen([sys.executable, 'src/uno_client.py', '0'],
+		#                                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+		config.Page = 2
--- page.py
+++ page.py
@@ -3,11 +3,10 @@ from src import button
 import subprocess, sys
 import pygame_textinput
 from src import config
-import hostPrompt, enterRoomSize
 
 pygame.init()
 
-# create display window
+#create display window
 SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 800
 TEXTBOX_WIDTH = 272
@@ -19,34 +18,16 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('UNO')
 
-
-#-----------------------------------
-# Host Prompt
-#-----------------------------------
-createRoom = pygame.image.load('assets/textures/createroom.png').convert_alpha()
-joinRoom = pygame.image.load('assets/textures/joinroom.png').convert_alpha()
-
-createButton = button.Button(100, 200, createRoom, 0.4)
-joinButton = button.Button(450, 200, joinRoom, 0.4)
-
-
-#-----------------------------------
-# Room size
-#-----------------------------------
-
-# loading images
+#load button images
 sample_button = pygame.image.load('assets/textures/2.png').convert_alpha()
 enterRoomSize_ = pygame.image.load('assets/textures/roomsize.png').convert_alpha()
 
-# Creating butttons
-enterRoomSizeHeader = button.Button(SCREEN_WIDTH/2-(enterRoomSize_.get_width()/2*0.4), SCREEN_HEIGHT/5, enterRoomSize_, 0.4)
+enterRoomSize = button.Button(SCREEN_WIDTH/2-(enterRoomSize_.get_width()/2*0.4), SCREEN_HEIGHT/5, enterRoomSize_, 0.4)
 
 roomSizeButtons = [[] for _ in range(3)]
 roomSizeButtonsFinal = [[] for _ in range(3)]
 roomSizeNames = [[] for _ in range(3)]
 
-
-# making room buttons
 count = 2
 for i in range(3):
     for j in range(3):
@@ -56,7 +37,6 @@ for i in range(3):
         count+=1
 
 w,h = -1.5*sample_button.get_width()*0.45,0
-
 for i in range(3):
     for j in range(3):
         roomSizeButtons[i][j] = button.Button(bPOsX + w, bPosY + h, roomSizeButtons[i][j], 0.4)
@@ -65,11 +45,7 @@ for i in range(3):
     w=-1.5*sample_button.get_width()*0.45
     h += sample_button.get_height()*0.45
 
-
-
-#-----------------------------------
-# Game Loop
-#-----------------------------------
+#game loop
 run = True
 config.waitQuit = 0
 
@@ -78,11 +54,15 @@ while run:
     screen.fill((202, 228, 241))
     events = pygame.event.get()
 
-    if config.Page == 0:
-        hostPrompt.display(screen, createButton, joinButton)
-    elif config.Page == 1:
-        enterRoomSizeHeader.draw(screen)
-        enterRoomSize.display(screen, roomSizeButtons, roomSizeButtonsFinal, roomSizeNames)
+    for i in range(3):
+        for j in range(3):
+            if roomSizeButtons[i][j].draw(screen):
+                roomSizeButtons[i][j] = roomSizeButtonsFinal[i][j]
+                selectedButton = roomSizeNames[i][j]
+                print(selectedButton)
+                config.waitQuit += 1
+
+    enterRoomSize.draw(screen)
 
     for event in events:
         # quit game
